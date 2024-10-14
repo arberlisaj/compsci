de69b614e1e18165cefc1672f59e56dda8c5154c
arberlisaj <the.arberlisaj@gmail.com>
2024-10-14 20:27:35 +0200

    feat: add mortgage calculator in Java

---
+++ calculator/MortgageReport.java
@@ -0,0 +1,28 @@
+package com.calculator;
+
+import java.text.NumberFormat;
+
+public class MortgageReport {
+  private final NumberFormat currency;
+  private final MortgageCalculator calculator;
+
+  public MortgageReport(MortgageCalculator calculator) {
+    this.calculator = calculator;
+    currency = NumberFormat.getCurrencyInstance();
+  }
+
+  public void printPaymentSchedule() {
+    System.out.println();
+    System.out.println("PAYMENT SCHEDULE");
+    for (double balance : calculator.getRemainingBalances())
+      System.out.println(currency.format(balance));
+  }
+
+  public void printMortgage() {
+    double mortgage = calculator.calculateMortgage();
+    String mortgageFormatted = currency.format(mortgage);
+    System.out.println();
+    System.out.println("MORTGAGE");
+    System.out.println("Monthly Payments: " + mortgageFormatted);
+  }
+}
