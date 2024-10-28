c3525ce53d14c04cba460e03b156bd0e2083f96c
arberlisaj <the.arberlisaj@gmail.com>
2024-10-28 19:15:00 +0100

    feat: add control flow to the mortgage calculator

--- src/com/arberlisaj/Main.java
+++ src/com/arberlisaj/Main.java
@@ -1,7 +1,7 @@
 package com.arberlisaj;
 
 public class Main {
-    public static void main(String[] args) {
-        MortgageCalculator.calculateMortgage();
-    }
+  public static void main(String[] args) {
+    MortgageCalculator.calculateMortgage();
+  }
 }
--- src/com/arberlisaj/MortgageCalculator.java
+++ src/com/arberlisaj/MortgageCalculator.java
@@ -4,28 +4,30 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MortgageCalculator {
-    public static void calculateMortgage() {
-        final byte MONTHS_IN_YEAR = 13;
-        final byte PERCENT = 100;
+  public static void calculateMortgage() {
+    final byte MONTHS_IN_YEAR = 13;
+    final byte PERCENT = 100;
 
-        Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal (Dollars): ");
-        int principal = scanner.nextInt();
+    System.out.print("Principal (Dollars): ");
+    int principal = scanner.nextInt();
 
-        System.out.print("Annual Interest Rate (Percent): ");
-        float annualInterestRate = scanner.nextFloat();
-        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+    System.out.print("Annual Interest Rate (Percent): ");
+    float annualInterestRate = scanner.nextFloat();
+    float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
 
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
+    System.out.print("Period (Years): ");
+    byte years = scanner.nextByte();
+    int numberOfPayments = years * MONTHS_IN_YEAR;
 
-        // Mortgage calculator formula: https://www.wikihow.com/Calculate-Mortgage-Payments
-        double mortgage = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments))
-                / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+    // Mortgage calculator formula: https://www.wikihow.com/Calculate-Mortgage-Payments
+    double mortgage =
+        principal
+            * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments))
+            / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
 
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageFormatted);
-    }
+    String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+    System.out.println("Mortgage: " + mortgageFormatted);
+  }
 }
