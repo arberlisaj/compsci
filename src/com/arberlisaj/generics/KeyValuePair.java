5a453dd03ed06604499f609d7f9fffb93f751298
arberlisaj <the.arberlisaj@gmail.com>
2024-11-06 18:18:40 +0100

    add generics

---
+++ src/com/arberlisaj/generics/KeyValuePair.java
@@ -0,0 +1,11 @@
+package com.arberlisaj.generics;
+
+public class KeyValuePair<K, V> {
+  private K key;
+  private V value;
+
+  public KeyValuePair(K key, V value) {
+    this.key = key;
+    this.value = value;
+  }
+}
---
+++ src/com/arberlisaj/generics/UserList.java
@@ -0,0 +1,6 @@
+package com.arberlisaj.generics;
+
+public class UserList {
+  private User[] items = new User[10];
+  private int count;
+}
