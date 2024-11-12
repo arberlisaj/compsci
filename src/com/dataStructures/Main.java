fce0668d79c63eb6bbc9e4ae069c7306c5468d96
arberlisaj <the.arberlisaj@gmail.com>
2024-11-12 23:19:35 +0100

    add arrays in data structures

--- src/com/dataStructures/Main.java
+++ src/com/dataStructures/Main.java
@@ -1,7 +1,14 @@
 package com.dataStructures;
 
+import com.dataStructures.arrays.Array;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Data structures");
-  }
+    var numbers = new Array(3);
+    numbers.insert(1);
+    numbers.insert(2);
+    numbers.insert(3);
+
+    numbers.print();
+  };
 }
