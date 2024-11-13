f4873cd976c0dbc38da1e391f444492317f0755e
arberlisaj <the.arberlisaj@gmail.com>
2024-11-13 23:51:26 +0100

    add linked lists in data structures

--- src/com/dataStructures/Main.java
+++ src/com/dataStructures/Main.java
@@ -1,6 +1,7 @@
 package com.dataStructures;
 
 import com.dataStructures.arrays.Array;
+import com.dataStructures.linkedLists.LinkedList;
 
 public class Main {
   public static void main(String[] args) {
@@ -8,7 +9,18 @@ public class Main {
     numbers.insert(1);
     numbers.insert(2);
     numbers.insert(3);
-
     numbers.print();
-  };
+
+    var list = new LinkedList();
+    list.addLast(1);
+    list.addLast(2);
+    list.addLast(3);
+    list.addLast(4);
+    list.addLast(5);
+
+    System.out.println("--------------");
+
+    System.out.println(list.getKthFromTheEnd(0));
+  }
+  ;
 }
