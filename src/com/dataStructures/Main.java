57f2a995ba378297a4781197838b644c2a003e0f
arberlisaj <the.arberlisaj@gmail.com>
2024-11-14 23:42:02 +0100

    feat: add stack implementation in `dataStructures` package

--- src/com/dataStructures/Main.java
+++ src/com/dataStructures/Main.java
@@ -2,25 +2,39 @@ package com.dataStructures;
 
 import com.dataStructures.arrays.Array;
 import com.dataStructures.linkedLists.LinkedList;
+import com.dataStructures.stacks.Stack;
 
 public class Main {
-  public static void main(String[] args) {
-    var numbers = new Array(3);
-    numbers.insert(1);
-    numbers.insert(2);
-    numbers.insert(3);
-    numbers.print();
+    public static void main(String[] args) {
+        var numbers = new Array(3);
+        numbers.insert(1);
+        numbers.insert(2);
+        numbers.insert(3);
+        numbers.print();
 
-    var list = new LinkedList();
-    list.addLast(1);
-    list.addLast(2);
-    list.addLast(3);
-    list.addLast(4);
-    list.addLast(5);
+        var list = new LinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
 
-    System.out.println("--------------");
+        System.out.println("--------------");
 
-    System.out.println(list.getKthFromTheEnd(0));
-  }
-  ;
+        System.out.println(list.getKthFromTheEnd(0));
+
+        System.out.println("--------------");
+
+        var stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.pop();
+
+        int a = stack.peek();
+        System.out.println(a);
+    }
+
+    ;
 }
---
+++ src/com/dataStructures/stacks/Reverser.java
@@ -0,0 +1,18 @@
+package com.dataStructures.stacks;
+
+import java.util.Stack;
+
+public class Reverser {
+  public String reverse(String input) {
+    if (input == null) throw new IllegalArgumentException();
+
+    Stack<Character> stack = new Stack<>();
+
+    for (char ch : input.toCharArray()) stack.push(ch);
+
+    StringBuffer reversed = new StringBuffer();
+    while (!stack.empty()) reversed.append(stack.pop());
+
+    return reversed.toString();
+  }
+}
