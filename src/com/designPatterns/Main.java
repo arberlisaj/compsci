a9a60a68532f724b771451f739d6284f8028be14
arberlisaj <the.arberlisaj@gmail.com>
2024-11-16 11:45:22 +0100

    feat: add memento pattern

--- src/com/designPatterns/Main.java
+++ src/com/designPatterns/Main.java
@@ -1,7 +1,25 @@
 package com.designPatterns;
 
+import com.designPatterns.memento.Editor;
+import com.designPatterns.memento.History;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Design Patterns");
-  }
+    public static void main(String[] args) {
+        var editor = new Editor();
+        var history = new History();
+
+        editor.setContent("a");
+        history.push(editor.createState());
+
+        editor.setContent("b");
+        history.push(editor.createState());
+
+        editor.setContent("c");
+        // we get b
+        editor.restore(history.pop());
+        // we get a
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
+    }
 }
